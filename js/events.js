9dc8905030edf0a8e9f380575695b80681f86976
Vaibhav Patel <vp932712@gmail.com>
2018-02-26 18:06:00 -0500

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,39 @@
 //define functions here
+function getIt(){
 
+  alert("hey!")
+}
+
+function frameIt(){
+  $( "img" ).addClass( "tasty" );
+
+}
+
+// function pressIt(){
+//   if (e.which === 71) {
+//       alert("You pressed 'g'!")
+//     }
+//   }
+
+
+function pressIt() {
+  $('#typing').on('keydown', function(e) {
+    // 'g' corresponds to 71
+    if (e.which === 71) {
+      alert("You pressed 'g'!")
+    }
+  })
+}
+function submitIt(){
+  alert("Your form is going to be submitted now.")
+}
 $(document).ready(function(){
 
-// call functions here
+  $('p').on('click', getIt())
 
+  $("img").load(frameIt())
+
+
+
+  $("form").submit(submitIt())
 });
